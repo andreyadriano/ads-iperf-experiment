e6b5c770cf193b50894c4d49b84a6ebaaa17568e
Andrey Adriano <dey2010dey@gmail.com>
2024-03-22 09:31:33 -0300

    pequenas correções

--- simulacao.py
+++ simulacao.py
@@ -5,10 +5,11 @@ ALG = ['cubic','reno']
 BER = ['1000000','100000']
 E2E_DELAY = ['10000','100000'] # 10ms e 100ms
 REPETICAO = 8
-TEMPO_EXECUCAO = '200'
+TEMPO_EXECUCAO = '1000'
 BANDA_UDP = ['5Mbps','10Mbps']
 ID_EXPERIMENTO = 'i1234'
 DADOS_CSV = 'data/cliente.csv'
+CENARIO_IMUNES = 'projeto.imn'
 
 ip_pc1 = '10.0.0.20'
 ip_pc2 = '10.0.2.20'
@@ -16,7 +17,6 @@ ip_pc3 = '10.0.3.20'
 ip_pc4 = '10.0.4.20'
 
 def inicia_imunes():
-    print('Iniciando simulação...')
     if os.path.exists('cliente.csv'):
         os.remove('cliente.csv')
     cmd = f"sudo imunes -b -e {ID_EXPERIMENTO} projeto.imn"
@@ -32,14 +32,19 @@ def prepara_cenario():
     subprocess.run(init_server_udp, shell=True)
     subprocess.run(init_server_tcp, shell=True)
     subprocess.run(init_cliente_udp, shell=True)
+    imprime_cabecalho()
     
 def encerra_imunes():
-    subprocess.run(f'sudo cleanupAll', shell=True).wait()
+    print('Encerrando simulações já existentes...')
+    subprocess.Popen(f'sudo cleanupAll', shell=True).wait()
 
 def simulacao():
+    print('Iniciando imunes...')
     inicia_imunes()
+    print('Preparando cenário...')
     prepara_cenario()
     
+    print('Iniciando simulação...')
     for rep in range(REPETICAO):
         for proto in ALG:
             for ber in BER:
@@ -48,12 +53,12 @@ def simulacao():
                         echo = f'echo -n {rep},{proto},{ber},{e2e},{banda}, >> {DADOS_CSV}'
                         subprocess.run(echo, shell=True)
                         
-                        vlink = f'sudo vlink -BER {ber} -dly {e2e} router1:router2@{ID_EXPERIMENTO}'
+                        print('comando vlink')
+                        vlink = f'sudo vlink -BER {ber} -d {e2e} router1:router2@{ID_EXPERIMENTO}'
                         subprocess.run(vlink, shell=True)
                         
                         init_cliente_tcp = f'sudo himage pc2@{ID_EXPERIMENTO} iperf -c {ip_pc1} -t {TEMPO_EXECUCAO} -Z {proto} -y C >> {DADOS_CSV}'
                         subprocess.run(init_cliente_tcp, shell=True)
-    
     encerra_imunes()
 
 encerra_imunes() # Limpa experimentos já existentes
